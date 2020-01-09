6aa2de7f4fdaefc5575e26492eb2cfa6cd8ec035
javier997 <javier260997@gmail.com>
2020-01-09 16:40:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-//'use strict';
-
+// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
+// for Dialogflow fulfillment library docs, samples, and to report issues
+'use strict';
+ 
 const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+const {dialogflow} = require('actions-on-google');
+process.env.DEBUG = 'dialogflow:debug'; 
 
-const {
-    Card,
-    Suggestion,
-  	SimpleResponse
-} = require('dialogflow-fulfillment');
-
-const {
-    dialogflow,
-    Carousel,
-    Image
-} = require('actions-on-google');
 const app = dialogflow();
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
 
-app.intent('Aena', conv=>{
-  conv.ask('Desde git');
+app.intent('aena', conv =>{
+conv.ask('Hola');
 });
